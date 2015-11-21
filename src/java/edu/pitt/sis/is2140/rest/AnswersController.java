0dec192c3e6d6402b42953c8ee4fea52eea41d54
Jiadong Wu <wuplus1992@gmail.com>
2015-11-21 12:34:49 -0500

    first commit

---
+++ src/java/edu/pitt/sis/is2140/rest/AnswersController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pitt.sis.is2140.rest;
+
+/**
+ *
+ * @author Wu
+ */
+public class AnswersController {
+
+    public AnswersController() {
+        //Initialize controller properties here or 
+        //in the Web Application Context
+
+        //setCommandClass(MyCommand.class);
+        //setCommandName("MyCommandName");
+        //setSuccessView("successView");
+        //setFormView("formView");
+    }
+
+}
